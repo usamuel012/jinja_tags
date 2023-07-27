bc111c0cad2afd9886ffa310c21dd2e0de84db05
usamuel012 <samuelsatyaanand1999@gmail.com>
2023-07-27 12:04:49 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def jinja(request):
-    j = {'name': 'Ethan Hunt', 'age': 32}
+    j = {'name': 'Ethan Hunt', 'age': 19}
     return render(request, 'jinja.html', context=j)
